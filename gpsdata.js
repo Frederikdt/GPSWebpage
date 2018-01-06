a50e9792be75393dad63317e67fe7eb34d2b9637
Jakob Ferdinandsen <jakob@jhaaf.dk>
2018-01-06 01:50:01 +0100

    Added dynamic showing of gps entries.

--- gpsdata.js
+++ gpsdata.js
@@ -1,15 +1,19 @@
 $(function () {
     setInterval(getData, 500);
 });
+
+
 function getData() {
     $.ajax({
         type: "GET",
         url: "https://api.joerha.dk/gps/2/10",
         success: function (data) {
-            updateView(data.data)
+            updateView(data.data);
+            updateMyMap(data.data)
         }
     });
 }
+
 function updateView(data) {
     var gpsRows = "";
     data.forEach(function (gpsEntry) {
@@ -27,23 +31,55 @@ function updateView(data) {
 }
 
 
-
-
-
- /*
+/*
 $(function(){
 
-    var $gpsdata = $('#gpsdata');
+   var $gpsdata = $('#gpsdata');
 
-    $.ajax({
-    type: 'GET',
-    url: 'https://joerha.dk/gps',
-    success: function(gpsdata) {
-    $.each(gpsdata, function(i, gpsdata){
-        $gpsdata.append()
-    });
-    }
-    });
+   $.ajax({
+   type: 'GET',
+   url: 'https://joerha.dk/gps',
+   success: function(gpsdata) {
+   $.each(gpsdata, function(i, gpsdata){
+       $gpsdata.append()
+   });
+   }
+   });
 
 });
 */
+var map = null;
+$(function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lng: 10.402370, lat: 55.403756}
+    });
+});
+var currentMarkers = [];
+
+function updateMyMap(gpsData) {
+    currentMarkers.forEach(function (marker) {
+        if (marker.delete) {
+            marker.setMap(null);
+        }
+    });
+
+    var preparedGpsData = [];
+    gpsData.forEach(function (element) {
+        preparedGpsData.push(new google.maps.LatLng(element.latitude, element.longitude));
+    });
+
+    currentMarkers.forEach(function (marker) {
+        marker.delete = true
+    });
+
+    preparedGpsData.forEach(function (element) {
+        var marker = new google.maps.Marker({
+            position: element,
+            map: map,
+            title: 'Hello World!'
+        });
+        currentMarkers.push(marker);
+    });
+
+}
